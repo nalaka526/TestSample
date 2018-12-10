9c17baefa9dec02277044746b5542355cfd3e115
Nalaka Thalagala <nth@tiqri.com>
2018-12-10 08:54:18 +0530

    Add project files.

---
+++ ClassLib/MethodsClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLib
+{
+    public class MethodsClass
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Tests/TestsClass.cs
@@ -0,0 +1,24 @@
+﻿using ClassLib;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class TestsClass
+    {
+        [Fact]
+        public void Test1()
+        {
+            var methods = new MethodsClass();
+            Assert.Equal(4, methods.Add(2, 2));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var methods = new MethodsClass();
+            Assert.Equal(4,  methods.Add(2, 2));
+        }
+
+    }
+}
